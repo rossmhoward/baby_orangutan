6aa5d8878fae0f159253fdc2db988ae47b3db62d
HollyGold89 <hollygoldsmiths@hotmail.co.uk>
2024-09-01 14:37:09 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,12 @@
-(function(){
-    //Bunch of code...
-    console.log('hello holly');
+window.onload = function() {
+console.log('hello holly');
     var terms = [
         'Palm Kernel',
         'Palm Kernel Oil',
         'Palm oils',
         'ever'
     ];
-    const check = 
-    window.document.documentElement.innerText.search('publishing') != '-1';
-    console.log(`here is an array of terms for you to start searching for: ${check}, [${terms}].`);
-    console.log(window,window.document, window.document.documentElement);
-})();
+    const content = window.document.body.innerText;
+    const check =  content.search('publishing') != '-1';
+    console.log('content', content , 'check' , check);
+}
